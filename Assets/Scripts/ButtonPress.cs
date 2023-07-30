f00727d38eab226aed18efd21651a605798c041d
SeMaiLiFezz <113405224+SeMaiLiFezz@users.noreply.github.com>
2023-07-30 17:19:34 +0800

    pushing

--- Assets/Scripts/ButtonPress.cs
+++ Assets/Scripts/ButtonPress.cs
@@ -2,12 +2,20 @@ using UnityEngine;
 
 public class ButtonPress : MonoBehaviour
 {
+    private SpriteRenderer spriteRenderer;
+    [SerializeField] public Sprite down, up;
+    private void Awake()
+    {
+        spriteRenderer= GetComponent<SpriteRenderer>();
+    }
     private void OnMouseDown()
     {
+        spriteRenderer.sprite = down;
         GameObject.Find("Target").GetComponent<TargetScript>().Click();
     }
     private void OnMouseUp()
     {
+        spriteRenderer.sprite = up;
         GameObject.Find("Target").GetComponent<TargetScript>().Release();
     }
 }
